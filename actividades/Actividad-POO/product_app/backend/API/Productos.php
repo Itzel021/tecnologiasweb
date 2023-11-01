c592bc1141e82b8fb1b69de5ed071c244235f2eb
ItzD <itzeeldaniela@gmail.com>
2023-11-01 00:42:55 -0600

    Act POO-php Funcion eliminar producto.

--- actividades/Actividad-POO/product_app/backend/API/Productos.php
+++ actividades/Actividad-POO/product_app/backend/API/Productos.php
@@ -87,6 +87,27 @@ class Productos extends DataBase{
             $this->conexion->close();
         }
     }
+    //FUNCION PARA ELIMINAR PRODUCTO
+    public function delete($id){
+        // SE CREA EL ARREGLO QUE SE VA A DEVOLVER EN FORMA DE JSON
+    $this->response = array(
+        'status'  => 'error',
+        'message' => 'La consulta falló'
+    );
+    // SE VERIFICA HABER RECIBIDO EL ID
+    if( isset($_POST['id']) ) {
+        $id = $_POST['id'];
+        // SE REALIZA LA QUERY DE BÚSQUEDA Y AL MISMO TIEMPO SE VALIDA SI HUBO RESULTADOS
+        $sql = "UPDATE productos SET eliminado=1 WHERE id = {$id}";
+        if ( $this->conexion->query($sql) ) {
+            $this->response['status'] =  "success";
+            $this->response['message'] =  "Producto eliminado";
+		} else {
+            $this->response['message'] = "ERROR: No se ejecuto $sql. " . mysqli_error($this->conexion);
+        }
+		$this->conexion->close();
+    } 
+    }
     
 }
 ?>
