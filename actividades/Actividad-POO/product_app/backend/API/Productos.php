c665234667e00de476262211f7c13b9138b8dd78
ItzD <itzeeldaniela@gmail.com>
2023-11-01 00:21:46 -0600

    Act POO-php Funcion buscar producto.

--- actividades/Actividad-POO/product_app/backend/API/Productos.php
+++ actividades/Actividad-POO/product_app/backend/API/Productos.php
@@ -35,6 +35,30 @@ class Productos extends DataBase{
         }
         $this->conexion->close();
     }
+    //FUNCION PARA BUSCAR PRODUCTOS
+    public function search($search){
+        // SE VERIFICA HABER RECIBIDO EL ID
+    if( isset($search) ){
+        // SE REALIZA LA QUERY DE BÚSQUEDA Y AL MISMO TIEMPO SE VALIDA SI HUBO RESULTADOS
+        $sql = "SELECT * FROM productos WHERE (id = '{$search}' OR nombre LIKE '%{$search}%' OR marca LIKE '%{$search}%' OR detalles LIKE '%{$search}%') AND eliminado = 0";
+        if ( $result = $this->conexion->query($sql) ) {
+            // SE OBTIENEN LOS RESULTADOS
+			$rows = $result->fetch_all(MYSQLI_ASSOC);
+            if(!is_null($rows)) {
+                // SE CODIFICAN A UTF-8 LOS DATOS Y SE MAPEAN AL ARREGLO DE RESPUESTA
+                foreach($rows as $num => $row) {
+                    foreach($row as $key => $value) {
+                        $this->response[$num][$key] = utf8_encode($value);
+                    }
+                }
+            }
+			$result->free();
+		} else {
+            die('Query Error: '.mysqli_error($this->conexion));
+        }
+		$this->conexion->close();
+        }
+    }
     
 }
 ?>
